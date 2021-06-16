74d322781bf03b6f7d3e1c9f3ca8220198b33176
Lee-sun-hang <dltjsgod0823@gmail.com>
2021-06-16 15:18:56 +0900

    등록된 게시판에 댓글 기능 추가
    1. 등록된 게시판에 답변 기능 추가

--- board/views.py
+++ board/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
@@ -14,6 +15,15 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'board/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('board:detail', question_id=question.id)
+
+
+
 
 # 제너릭 뷰 예시
 # class IndexView(generic.ListView):
