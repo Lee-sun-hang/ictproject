68796496510a500eaa35cab216a6402eacfaef7b
Lee-sun-hang <dltjsgod0823@gmail.com>
2021-06-16 16:59:55 +0900

    사용자 로그인, 로그아웃 폼 기능 추가
    1. 사용자 로그인, 로그아웃 폼 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,6 @@ from board import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),
 ]
