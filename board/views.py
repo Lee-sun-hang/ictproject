705c54c1af45aa4834dedc25488387fa1a5035f4
Lee-sun-hang <dltjsgod0823@gmail.com>
2021-06-16 16:10:24 +0900

    질문등록 버튼 추가 및 에러 사항 예외처리
    1. 게시판 리스트 페이지 하단에 질문등록 버튼 추가
    2. 등록,답변, 입력가 부재시 에러 메세지 노출 기능 추가

--- board/views.py
+++ board/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
-# Create your views here.
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -19,16 +19,37 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('board:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'board/question_detail.html', context)
 
 
-
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('board:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'board/question_form.html', context)
 
 # 제너릭 뷰 예시
 # class IndexView(generic.ListView):
 #     """
-#     pybo 목록 출력
+#     목록 출력
 #     """
 #     def get_queryset(self):
 #         return Question.objects.order_by('-create_date')
@@ -36,6 +57,6 @@ def answer_create(request, question_id):
 #
 # class DetailView(generic.DetailView):
 #     """
-#     pybo 내용 출력
+#     내용 출력
 #     """
 #     model = Question
