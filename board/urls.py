85df657e5f7d575624a06186583d9323c52d0325
Lee-sun-hang <dltjsgod0823@gmail.com>
2021-06-16 15:03:49 +0900

    게시판 데이터 리스트 및 디테일 출력
    1. template 안에 html 뿌려지도록 html 파일 생성
    2. 게시판 리스트 페이지 데이터 출력
    3. 게시판 detail 페이지 데이터 출력

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
+
+# 제너릭 뷰 예시
+# from django.urls import path
+#
+# from . import views
+#
+# app_name = 'pybo'
+# urlpatterns = [
+#     path('', views.IndexView.as_view()),
+#     path('<int:pk>/', views.DetailView.as_view()),
+# ]
--- board/views.py
+++ board/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요. 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'board/question_list.html', context)
+
+
+def detail(request, question_id):
+    # 페이지가 없을 시 에러페이지 띄움
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'board/question_detail.html', context)
+
+# 제너릭 뷰 예시
+# class IndexView(generic.ListView):
+#     """
+#     pybo 목록 출력
+#     """
+#     def get_queryset(self):
+#         return Question.objects.order_by('-create_date')
+#
+#
+# class DetailView(generic.DetailView):
+#     """
+#     pybo 내용 출력
+#     """
+#     model = Question
