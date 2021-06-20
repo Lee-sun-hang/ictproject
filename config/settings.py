784e62059e0508471e9866247742ae77d542a416
Lee-sun-hang <dltjsgod0823@gmail.com>
2021-06-20 19:09:47 +0900

    로컬 ip allowd hosts 추가
    - 로컬 ip allowd hosts 추가

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ DB_PORT = get_secret("db_port")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
